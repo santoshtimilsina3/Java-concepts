94cc079daa69ca5004c05b46cc694c21ea851044
santosh timilsina <santos.timil@gmail.com>
2023-08-15 11:26:07 +0545

    filter

--- Stream/src/Main.java
+++ Stream/src/Main.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Main {
 
@@ -20,5 +21,18 @@ public class Main {
 
         Double average = personList.stream().mapToDouble(data-> Double.parseDouble(String.valueOf(data.age))).average().getAsDouble();
         System.out.println(average);
+
+
+        //Write code using the Stream API to filter the productList and get a list of products with a price greater than $1000?
+        List<Product> productList = new ArrayList<>();
+        productList.add(new Product("Laptop", 1200));
+        productList.add(new Product("Phone", 800));
+        productList.add(new Product("Tablet", 500));
+        productList.add(new Product("icma", 1500));
+
+        List<Product> product1000price = productList.stream().filter(product -> product.price >1000).collect(Collectors.toList());
+        product1000price.forEach(product -> System.out.println(product.price));
+
+
     }
 }
