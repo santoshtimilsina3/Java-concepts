f3aba87b159c3ead0f77a0f2588a0b74e9d450af
santosh timilsina <santos.timil@gmail.com>
2023-08-15 11:15:37 +0545

    average-stream

--- Stream/src/Main.java
+++ Stream/src/Main.java
@@ -5,8 +5,20 @@ public class Main {
 
     public static void main(String[] args) {
 
+        //How would you use the Stream API to find the sum of all integers in the integerList?
         List<Integer> integerList = new ArrayList<>(List.of(5, 12, 3, 8, 20, 7, 15));
        Long finalSum  = integerList.stream().mapToLong(data -> Long.valueOf(data)).sum();
         System.out.println(finalSum);
+
+
+        //What Stream API operation would you use to find the average age of the personList?
+
+        List<Person> personList = new ArrayList<>();
+        personList.add(new Person("Alice", 28));
+        personList.add(new Person("Bob", 35));
+        personList.add(new Person("Carol", 22));
+
+        Double average = personList.stream().mapToDouble(data-> Double.parseDouble(String.valueOf(data.age))).average().getAsDouble();
+        System.out.println(average);
     }
 }
