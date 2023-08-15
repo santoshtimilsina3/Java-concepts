0f2b94ec441fb578439e31116c3c4a0b827a3d2f
santosh timilsina <santos.timil@gmail.com>
2023-08-15 11:03:01 +0545

    sum-in-stream

--- Stream/src/Main.java
+++ Stream/src/Main.java
@@ -1,6 +1,12 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
+
     public static void main(String[] args) {
 
-        System.out.println("Hello world!");
+        List<Integer> integerList = new ArrayList<>(List.of(5, 12, 3, 8, 20, 7, 15));
+       Long finalSum  = integerList.stream().mapToLong(data -> Long.valueOf(data)).sum();
+        System.out.println(finalSum);
     }
 }
