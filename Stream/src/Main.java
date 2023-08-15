3bfc6e58cd8a86522e9a9f478bd3a47da5d3f21e
santosh timilsina <santos.timil@gmail.com>
2023-08-15 12:23:15 +0545

    max-sorted-max-limit

--- Stream/src/Main.java
+++ Stream/src/Main.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -32,6 +33,20 @@ public class Main {
 
         List<Product> product1000price = productList.stream().filter(product -> product.price >1000).collect(Collectors.toList());
         product1000price.forEach(product -> System.out.println(product.price));
+
+        // Question 2: Using the Stream API, how can you find the product with the highest price?
+        Comparator<Product> maxProduct = (firstProduct,secondProduct) -> Double.compare(firstProduct.getPrice(),secondProduct.getPrice());
+       Product maxPriceProduct =  productList.stream().max(maxProduct).orElse(null);
+        System.out.println("product with max price  s" + maxPriceProduct.price);
+
+        double price = productList.stream().sorted(Comparator.comparing(Product::getPrice).reversed()).collect(Collectors.toList()).get(0).getPrice();
+        System.out.println("max price "  + price);
+
+        /** Question: You have a list of Product objects, each containing a name and a price.
+         How can you use the Stream API to find the names of the top N products with the highest prices? */
+
+       List<String> names =  productList.stream().sorted(Comparator.comparing(Product::getPrice).reversed()).map(name-> name.getName()).limit(2).collect(Collectors.toList());
+        System.out.println(names.toString());
 
 
     }
