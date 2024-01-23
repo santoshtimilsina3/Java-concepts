a91cb108b9f38545c14b52bf349b63e82bfe128e
santosh timilsina <santos.timil@gmail.com>
2024-01-23 22:26:55 +0545

    address-person-added

---
+++ reflection/src/jsonserilizer/classes/Address.java
@@ -0,0 +1,11 @@
+package jsonserilizer.classes;
+
+public class Address {
+    private final String street;
+    private final int apartmentNo;
+
+    public Address(String street, int apartmentNo) {
+        this.street = street;
+        this.apartmentNo = apartmentNo;
+    }
+}
