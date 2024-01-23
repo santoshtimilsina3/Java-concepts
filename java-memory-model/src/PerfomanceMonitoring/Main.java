d76a138d7fc9d7172db8e425d725177e13845fd8
santosh timilsina <santos.timil@gmail.com>
2024-01-23 23:49:30 +0545

    code-cache-size-information

--- java-memory-model/src/PerfomanceMonitoring/Main.java
+++ java-memory-model/src/PerfomanceMonitoring/Main.java
@@ -1,13 +1,15 @@
 package PerfomanceMonitoring;
 
 public class Main {
-/* put -XX:+PrintCompilation
-in VM argument to seee the native code
-* */
-	public static void main(String[] args) {
-		PrimeNumbers primeNumbers = new PrimeNumbers();
-		Integer max = Integer.parseInt(args[0]);
-		primeNumbers.generateNumbers(max);
-	}
+    /* put -XX:+PrintCompilation --> in VM argument to seee the native code
+    put -XX:+PrintCodeCache --> in VM argument to see the CodeCache Size
+    put -XX:ReservedCodeCacheSize=20m  --> in VM argument to increase  the CodeCache Size
+
+    * */
+    public static void main(String[] args) {
+        PrimeNumbers primeNumbers = new PrimeNumbers();
+        Integer max = Integer.parseInt(args[0]);
+        primeNumbers.generateNumbers(max);
+    }
 
 }
