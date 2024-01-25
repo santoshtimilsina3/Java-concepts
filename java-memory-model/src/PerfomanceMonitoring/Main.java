34339083605f4082d94d2abc9bf48b6c325fd1e6
santosh timilsina <santos.timil@gmail.com>
2024-01-26 01:21:36 +0545

    no-of-thread-to-compile-code-&-no-of-times-to-natively-compile

--- java-memory-model/src/PerfomanceMonitoring/Main.java
+++ java-memory-model/src/PerfomanceMonitoring/Main.java
@@ -4,7 +4,8 @@ public class Main {
     /* put -XX:+PrintCompilation --> in VM argument to seee the native code
     put -XX:+PrintCodeCache --> in VM argument to see the CodeCache Size
     put -XX:ReservedCodeCacheSize=20m  --> in VM argument to increase  the CodeCache Size
-
+    put --XX:CICompilerCount=4 to increase/decrease the no.of thread that compiles our code
+    put --XX:CompilerThreshold=4 to increase/decrease the times that the compiler changes to native compile if it runs multiple times
     * */
     public static void main(String[] args) {
         PrimeNumbers primeNumbers = new PrimeNumbers();
